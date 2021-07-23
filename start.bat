efb98637e01dac926330922b36f3c99e3afb835b
JSJVESKN28 <83856241+JSJVESKN28@users.noreply.github.com>
2021-07-23 00:32:34 -0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -3,7 +3,7 @@
 
 net config server /srvcomment:"Windows Server 2019 By MATHEUS H4CK3R" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-net user administrator vm1234 /add >nul
+net user administrator RDP-GITHUB1234 /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 sc config Audiosrv start= auto >nul
@@ -15,6 +15,6 @@ echo Success!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
 echo Username: administrator
-echo Password: JohnTech1234
+echo Password: RDP-GITHUB1234
 echo You can login now.
 ping -n 10 127.0.0.1 >nul
